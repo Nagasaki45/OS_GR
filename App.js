4dd13e73d5e1bca362c911048aba7bd7dea0ab11
Tom Gurion <nagasaki45@gmail.com>
2021-05-08 02:31:15 +0100

    It's working! :-)

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Linking, StyleSheet, View, Text } from "react-native";
 
-const App = () => {
-  var LatLon = require('mt-latlon');
-  var OsGridRef = require('mt-osgridref');
-  var latlon = new LatLon(51.5136, -0.0983);
-  var point = OsGridRef.latLongToOsGrid(latlon);
-  return <Text style={styles.title}>{point.toString()}</Text>;
+const LatLon = require("mt-latlon");
+const OsGridRef = require("mt-osgridref");
+const geoRegex = /geo:(-?[0-9]+.[0-9]+),(-?[0-9]+.[0-9]+)/;
+
+const App = (props) => {
+
+  const [gr, setGr] = useState(null);
+
+  const handleGeoEvent = (event) => {
+    handleGeo(event.url);
+  };
+
+  // Handle geo and set gr
+  const handleGeo = (url) => {
+    const match = url.match(geoRegex);
+    const latlon = new LatLon(match[1], match[2]);
+    const point = OsGridRef.latLongToOsGrid(latlon);
+    setGr(point.toString());
+  };
+
+  // Register geo handler on component mount, remove on dismount
+  useEffect(() => {
+    Linking.addEventListener("url", handleGeoEvent);
+    return (() => {
+      Linking.removeEventListener("url", handleGeoEvent);
+    })
+  })
+
+  // Get the deep link used to open the app
+  Linking.getInitialURL().then((initialUrl) => {
+    if (initialUrl && !gr) {
+      handleGeo(initialUrl)
+    }
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{gr}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   title: {
     fontSize: 40,
-    color: '#fff',
+    color: "#fff",
   },
 });
 
